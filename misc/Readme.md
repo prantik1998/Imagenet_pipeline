e78b6c7088c042afb91bcac1415d10380b785504
Mayank Kumar Singh <mayank25031998@gmail.com>
2019-06-09 12:40:45 +0900

    removing_misc

--- misc/Readme.md
+++
@@ -1,5 +0,0 @@
-Replaced
-
-Data
-Image Size - 256 to 512
-Factor from 5 - 1.5
--- misc/visualise_progress.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import torch
-import matplotlib.pyplot as plt
-
-x = torch.load('0_0_info_checkpoint.pth.tar')
-
-print(np.mean(x['Acc']))
